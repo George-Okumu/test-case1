eaf9451b9a93e9e399d49c0ff4d4bd72d710713c
George-Okumu <gokumu12@gmail.com>
2025-03-18 11:52:30 +0300

    now am tired

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
+import React from 'react';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
-import LandingPage1 from "./components/LandingPage1";
-import CustomerPortal from "./components/customerPortal/CustomerPortal";
-import Draft from "./components/fundiPortfolio/fundiLanding/Draft";
-import NotFound from "./components/errorPage/NotFound";
-import LoginScreen from "./components/Login";
-// import TrackProgress from "./components/fundiPortfolio/active/TrackProgress";
-// import ProBillSummary from "./components/customerPortal/professionalPortal/requisitions/ProBillSummary";
-// import ProTeam from "./components/customerPortal/professionalPortal/requisitions/ProTeam";
-// import CustomerAccountSideNav from "./components/customerPortal/manageAccount/CustomerAccountSideNav";
-// import ShopAppNavBar from "./components/Navigation/ShopAppNavBar";
-
+import Home from './pages/Home';
+import LoginScreen from './components/Login';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<LandingPage1 />}>
-      <Route index element={<LandingPage1 />} />
-      <Route path="/customer" element={<CustomerPortal />} />
-      <Route path="/draft" element={<Draft />} />
+    <Route path="/" element={<Header />}>
+      <Route index element={<Home />} />
       <Route path="login" element={<LoginScreen />} />
-      <Route path="*" element={<NotFound />} />
-
     </Route>
   )
 )
 
-function App() {
-  return (
-    <RouterProvider router={router}/>
+function App({routes}) {
 
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
   );
 }
 
